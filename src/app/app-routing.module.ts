8b84e22004f126c4097bc9d2510a621280d19ade
Noemin Lin <noeminlin@gmail.com>
2023-01-04 17:27:00 +0800

    button, input, bage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { FormComponent } from './form/form.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
   {
     path: 'navigation', component: NavigationComponent,
+  }, {
+    path: 'form', component: FormComponent,
+  }, {
+    path: 'table', component: TableComponent,
+  }, {
+    path: 'dashboard', component: DashboardComponent,
+  }, {
+    path: 'dragdrop', component: DragDropComponent,
   }, {
     path: '**', component: NavigationComponent
   }];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,41 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
+import { ColorComponent } from './color/color.component';
+import { FormComponent } from './form/form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TableComponent } from './table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { DragDropComponent } from './drag-drop/drag-drop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatBadgeModule } from '@angular/material/badge';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    ButtonComponent,
+    InputComponent,
+    ColorComponent,
+    FormComponent,
+    TableComponent,
+    DashboardComponent,
+    DragDropComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +56,22 @@ import { MatListModule } from '@angular/material/list';
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatMenuModule,
+    DragDropModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatBadgeModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
